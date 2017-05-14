778641a7d9ceedf63058c17233e5112c5a938792
Jacky Zhang <jakzhang@ucdavis.edu>
2017-05-13 17:25:15 -0700

    Update PCA

--- Sberbank.R
+++ Sberbank.R
@@ -1,7 +1,11 @@
 #Sberbank Russian Housing Market
-library(Hmisc)
+library(data.table)
 library(tidyverse)
-library(caret)
+library(lubridate)
+library(scales)
+library(corrplot)
+library(DT)
+library(Hmisc)
 color<-"cornflowerblue"
 
 #Load data
@@ -14,11 +18,43 @@ names(dtrain)
 head(dtrain, 10)
 tail(dtrain, 10)
 
-#Histotram of price_doc (outcome variable)
+##Distribution of outcome variable
+#Histotram of price_doc
 ggplot(aes(x=price_doc), data=dtrain) + 
   geom_histogram(fill=color, bins=20) + 
   ggtitle('Distribution of sale price')
 
+#Histogram of log(price_doc)
 ggplot(aes(x=log10(price_doc)), data=dtrain) + 
   geom_histogram(fill=color, bins=20) + 
   ggtitle('Distribution of log(sale price)')
+
+#
+dtrain %>% 
+  filter(build_year > 1691 & build_year < 2018) %>% 
+  ggplot(aes(x=build_year, y=full_sq)) + 
+  geom_point() + 
+  ggtitle('Full area vs. year built')
+
+
+#Correlation matrix 
+internal_chars <- c('full_sq', 'life_sq', 'floor', 'max_floor', 'build_year', 'num_room', 
+                    'kitch_sq', 'state', 'price_doc')
+
+corrplot(cor(dtrain[, internal_chars], use="complete.obs"))
+
+
+#Prepare for PCA by subsetting out non-numerical variables
+col.class<-sapply(dtrain, class)
+col.class.nonchar<-names(which(col.class!="character"))
+
+dtrain.pca<-dtrain[,col.class.nonchar]
+
+
+# Pricipal Components Analysis entering raw data and extracting PCs from the correlation matrix 
+fit <- princomp(~ ., data = dtrain.pca, cor = TRUE)
+summary(fit) # print variance accounted for 
+loadings(fit) # pc loadings 
+plot(fit,type="lines") # scree plot 
+fit$scores # the principal components
+biplot(fit)
